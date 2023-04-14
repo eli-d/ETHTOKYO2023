c5514267865562e91fb95e503bca13427e55c8a8
eli-d <64763513+eli-d@users.noreply.github.com>
2023-04-14 22:44:12 +0930

    oops

--- util.ts
+++ util.ts
@@ -1,4 +1,7 @@
+import ethers from "ethers";
+
 enum Trust {
+    NONE,
     // could be nice to have PARTIAL_ONE/PARTIAL_TWO
     // for UX on how trusted an account is
     // alt: just have PARTIAL
