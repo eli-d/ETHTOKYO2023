6dfafd14cd2188c8c419ee3ce35f7f17b2d11d58
eli-d <64763513+eli-d@users.noreply.github.com>
2023-04-14 22:38:20 +0930

    scaffold typescript chain interaction functions

---
+++ util.ts
@@ -0,0 +1,41 @@
+enum Trust {
+    NONE,
+    PARTIAL_ONE,
+    PARTIAL_TWO,
+    FULL
+};
+
+type Account = {
+    address: string;
+    trust: Trust;    
+}
+
+export const getAccounts = async(): Promise<Account[]> => {
+
+}
+
+// b64
+export const createOwnerProof = async(): Promise<string> => {
+
+}
+
+// return address that signed
+export const importOwnerProof = async(proof: string): Promise<string> => {
+
+}
+
+// fetch trust
+export const lookupAddress = async(address: string): Promise<Account> => {
+    return {
+        address,
+        trust: Trust.NONE
+    }
+}
+
+export const trustAddress = async(address: string) => {
+
+}
+
+export const revokeTrust = async(address: string) => {
+
+}
