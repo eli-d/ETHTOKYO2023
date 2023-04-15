3e8b55d09630574d89d36931e3d6bef501cb3b06
Alfie Edgeworth <0xalfie@gmail.com>
2023-04-15 17:35:38 +0900

    added family connectkit

--- confide/pages/_app.tsx
+++ confide/pages/_app.tsx
@@ -1,6 +1,19 @@
 import '@/styles/globals.scss'
 import type { AppProps } from 'next/app'
 
+import { WagmiConfig, createClient } from "wagmi";
+import { ConnectKitProvider, getDefaultClient } from "connectkit";
+
+const client = createClient(
+  getDefaultClient({
+    appName: "confide",
+  }),
+);
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <WagmiConfig client={client}>
+    <ConnectKitProvider>
+      <Component {...pageProps} />
+    </ConnectKitProvider>
+  </WagmiConfig>
 }
